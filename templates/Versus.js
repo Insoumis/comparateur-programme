9ba550e0847eda334a38aa94fa364b61737145a9
Goutte <antoine.goutenoir@gmail.com>
2017-03-11 05:19:13 +0100

    Make logos for each of the political parties. For aesthetic symmetry with the φ.

--- templates/Versus.js
+++ templates/Versus.js
@@ -22,14 +22,14 @@ const Versus = ({ data }) => (
           <div className="vs">V<span>S</span></div>
           <Col md={6}>
             <div className="logo">
-              <img src={prefixLink(`/assets/${data.alias}.png`)} alt={`Logo ${data.title}`} />
+              <img src={prefixLink(`/assets/${data.alias}-logo.png`)} alt={`Logo ${data.title}`} />
             </div>
             <h2>{data.title}</h2>
             <div className="candidat">avec {data.candidat}</div>
           </Col>
           <Col md={6}>
             <div className="logo">
-              <img src={prefixLink('/assets/fi.png')} alt="Logo de la France insoumise"/>
+              <img src={prefixLink('/assets/fi-logo.png')} alt="Logo de la France insoumise"/>
             </div>
             <h2>La France insoumise</h2>
             <div className="candidat">avec Jean-Luc Mélenchon</div>
